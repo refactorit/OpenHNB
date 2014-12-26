99396ff984c86d03d32a89b1b3012da33ffedc63
Filip Defar <filip.defar@gmail.com>
2014-12-26 14:10:39 +0100

    Add currency model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Currency.create([
+  { country: "Australija", code: "036", name: "AUD" },
+  { country: "Austrija", code: "040", name: "ATS" },
+  { country: "Belgija", code: "056", name: "BEF" },
+  { country: "Kanada", code: "124", name: "CAD" },
+  { country: "Češka", code: "203", name: "CZK" },
+  { country: "Danska", code: "208", name: "DKK" },
+  { country: "Finska", code: "246", name: "FIM" },
+  { country: "Francuska", code: "250", name: "FRF" },
+  { country: "Njemačka", code: "280", name: "DEM" },
+  { country: "Grčka", code: "300", name: "GRD" },
+  { country: "Mađarska", code: "348", name: "HUF" },
+  { country: "Irska", code: "372", name: "IEP" },
+  { country: "Italija", code: "380", name: "ITL" },
+  { country: "Japan", code: "392", name: "JPY" },
+  { country: "Nizozemska", code: "528", name: "NLG" },
+  { country: "Norveška", code: "578", name: "NOK" },
+  { country: "Portugal", code: "620", name: "PTE" },
+  { country: "Slovačka", code: "703", name: "SKK" },
+  { country: "Slovenija", code: "705", name: "SIT" },
+  { country: "Španjolska", code: "724", name: "ESP" },
+  { country: "Švedska", code: "752", name: "SEK" },
+  { country: "Švicarska", code: "756", name: "CHF" },
+  { country: "Velika Britanija", code: "826", name: "GBP" },
+  { country: "SAD", code: "840", name: "USD" },
+  { country: "Evropska Zajednica", code: "954", name: "XEU" },
+  { country: "EMU", code: "978", name: "EUR" },
+  { country: "Poljska", code: "985", name: "PLN" },
+])
